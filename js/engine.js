daf362a893ffc2452cc271be8e51659a4ac633ce
Joshua Chen <jcgter777@gmail.com>
2023-07-12 12:57:31 -0400

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,10 +1,9 @@
 let wheel = document.querySelector('.wheel');
-let spinBtn = document.querySelector('.spinBtn');
 
 let val = 0;
 let current = 0;
 
-spinBtn.onclick = function () {
+document.querySelector('.spinBtn').onclick = function () {
     
     let rand = Math.ceil(Math.random() * 8);
     val += rand * 45 + 3600;
@@ -20,6 +19,8 @@ spinBtn.onclick = function () {
     }, 5000);
 
 }
+
+let wheel1 = new Array("What is your favorite animal?", "What is your favorite color?", "What is your favorite sport?", "What kind of music do you like to listen to?", "What is your favorite food?", "Who is your least favorite Panda Programmer teacher (pick Ms. Gloria for demonstration purposes)?", "What is your favorite travel location?", "What is your favorite hobby?"); 
 
 function promptQ(index) {
     console.log("bozo");
