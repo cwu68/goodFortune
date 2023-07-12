ef31341479a6a84640fa0b9d1a6663333b65a018
Joshua Chen <jcgter777@gmail.com>
2023-07-12 10:16:38 -0400

    Set up the skeleton of the function to pop up question modal

--- js/engine.js
+++ js/engine.js
@@ -1,16 +1,26 @@
 let wheel = document.querySelector('.wheel');
 let spinBtn = document.querySelector('.spinBtn');
 
-let rand = Math.ceil(Math.random() * 8);
-let val = rand * 45 + 3600;
+let val = 0;
 let current = 0;
 
 spinBtn.onclick = function () {
     
+    let rand = Math.ceil(Math.random() * 8);
+    val += rand * 45 + 3600;
     wheel.style.transform = "rotate(" + -val + "deg)";
 
     current = (current + rand) % 8;
-    console.log(current)
+    console.log(current);
     rand = Math.ceil(Math.random() * 8);
-    val += rand * 45 + 3600;
+
+    setTimeout(() => {
+        promptQ(current);
+        
+    }, 5000);
+
+}
+
+function promptQ(index) {
+    console.log("bozo");
 }
