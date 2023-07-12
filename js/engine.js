a1953c65c3546ff0a16b40877575cfbeee62feab
Joshua Chen <jcgter777@gmail.com>
2023-07-12 10:00:04 -0400

    Cleaned up spinner to eliminate ambiguous results

--- js/engine.js
+++ js/engine.js
@@ -1,9 +1,16 @@
 let wheel = document.querySelector('.wheel');
 let spinBtn = document.querySelector('.spinBtn');
-let val = Math.ceil(Math.random() * 3600);
+
+let rand = Math.ceil(Math.random() * 8);
+let val = rand * 45 + 3600;
+let current = 0;
 
 spinBtn.onclick = function () {
-    wheel.style.transform = "rotate(" + val + "deg)";
+    
+    wheel.style.transform = "rotate(" + -val + "deg)";
 
-    val += Math.ceil(Math.random() * 3600);
+    current = (current + rand) % 8;
+    console.log(current)
+    rand = Math.ceil(Math.random() * 8);
+    val += rand * 45 + 3600;
 }
