29a512309d665bffce9bdbad864829464f46a6f0
Joshua Chen <jcgter777@gmail.com>
2023-07-13 18:53:14 -0400

    I need help...

--- js/engine.js
+++ js/engine.js
@@ -1,7 +1,15 @@
-    let wheel = document.querySelector('.wheel');
+const wheel = document.querySelector('.wheel');
+const prompts = [
+    ["What is your favorite animal?", "What is your favorite color?", "What is your favorite sport?", "What kind of music do you like to listen to?", "What is your favorite food?", "Who is your least favorite Panda Programmer teacher (pick Ms. Gloria for demonstration purposes)?", "What is your favorite travel location?", "What is your favorite hobby?"],
+    ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8"],
+    ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8"]
+];
 
+console.log(prompts)
 let val = 0;
+let question = 0;
 let current = 0;
+let results = [];
 
 document.querySelector('.spinBtn').onclick = function () {
     
@@ -9,25 +17,26 @@ document.querySelector('.spinBtn').onclick = function () {
     val += rand * 45 + 3600;
     wheel.style.transform = "rotate(" + -val + "deg)";
 
-    current = (current + rand) % 8;
-    console.log(current);
+    question = (question + rand) % 8;
     rand = Math.ceil(Math.random() * 8);
 
     setTimeout(() => {
-        promptQ(current);
-        
-    }, 5000);
 
+        let choice = document.querySelector('.popuptext'); 
+        choice.innerText = prompts[current][question];
+
+        let container = document.querySelector('.popup');
+        container.style.display = 'flex';
+
+        document.getElementById('field').addEventListener('keypress', function (e) {
+            if (e.key == 'Enter') {
+                console.log(current);
+                results[current] = document.getElementById('field').value;
+                document.getElementById('field').value = '';
+                container.style.display = 'none';
+                current++;
+                console.log("WHY")
+            }
+        });
+    }, 3000);
 }
-
-let wheel1 = new Array("What is your favorite animal?", "What is your favorite color?", "What is your favorite sport?", "What kind of music do you like to listen to?", "What is your favorite food?", "Who is your least favorite Panda Programmer teacher (pick Ms. Gloria for demonstration purposes)?", "What is your favorite travel location?", "What is your favorite hobby?"); 
-
-function promptQ(index) {
-    let choice = document.querySelector('.popuptext'); 
-    choice.innerText = wheel1[index];
-
-    let container = document.querySelector('.popup');
-
-    container.style.display = 'flex'
-}
- 
