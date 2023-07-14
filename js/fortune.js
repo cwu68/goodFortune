b9c9e539e0fb5635efbe5064e9fc5851d39e8428
Joshua Chen <jcgter777@gmail.com>
2023-07-14 11:04:58 -0400

    Added cookie sliding animation

--- js/fortune.js
+++ js/fortune.js
@@ -1,6 +1,10 @@
 document.querySelector('.fortune').innerText = sessionStorage.getItem('fortune')
 
 document.querySelector('.cookie').onclick = function () {
-    document.querySelector('.container').style.display = 'block';
-    document.querySelector('.cookie').style.display = 'none';
+    document.querySelector('.cookie').style.animation = 'slideout 3s';
+    setTimeout(() => {
+        document.querySelector('.cookie').style.transform = 'translateX(100%)';
+        document.querySelector('.container').style.display = 'block';
+        // document.querySelector('.cookie').style.display = 'none';
+    }, 3000);
 }
