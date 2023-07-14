606b1ba27c13670092882cc1954f62cb595b652b
Joshua Chen <jcgter777@gmail.com>
2023-07-14 10:58:48 -0400

    Added fortune cookie to final result

--- js/fortune.js
+++ js/fortune.js
@@ -1,1 +1,6 @@
 document.querySelector('.fortune').innerText = sessionStorage.getItem('fortune')
+
+document.querySelector('.cookie').onclick = function () {
+    document.querySelector('.container').style.display = 'block';
+    document.querySelector('.cookie').style.display = 'none';
+}
